d9872ee8e10a9d8e1ebb834c9361b3aa0e00e7af
DimejiAre <olardi2@gmail.com>
2019-09-02 15:10:43 +0100

    optimize digital timer function logic

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -3,29 +3,30 @@ let counter = 0;
 // setInterval(function(){document.querySelector("#msTens").textContent = counter += 1}, 100);
 
 function startTimer(){
-    let msTenCounter = 0;
-    let msHundredsCounter = 0;
-    let secondCounter = 0;
-    let tenSecondCounter = 0;
+    let msTenCounter = 1;
+    let msHundredsCounter = 1;
+    let secondCounter = 1;
     setInterval(function(){
-        let addition = msTenCounter += 1
-        document.querySelector("#msTens").textContent = addition;
-        if(msTenCounter === 9){
+        if(msTenCounter > 9){
             msTenCounter = 0;
         }
+        document.querySelector("#msTens").textContent = msTenCounter;
+        msTenCounter += 1
     }, 10);
     setInterval(function(){
-        document.querySelector("#msHundreds").textContent = msHundredsCounter += 1;
-        if(msHundredsCounter === 9){
+        if(msHundredsCounter > 9){
             msHundredsCounter = 0;
         }
+        document.querySelector("#msHundreds").textContent = msHundredsCounter;
+        msHundredsCounter += 1
     }, 100);
     setInterval(function(){
-        document.querySelector("#secondOnes").textContent = secondCounter += 1;
-        if(secondCounter === 9){
+        if(secondCounter > 9){
             secondCounter = 0;
         }
+        document.querySelector("#secondOnes").textContent = secondCounter;
+        secondCounter += 1
     }, 1000);
-    document.querySelector("#secondTens").textContent = secondCounter;
+    document.querySelector("#secondTens").textContent = 0;
 }
 startTimer();
