f18a78a9d9485c01e0c23223ae5424622f004d30
DimejiAre <olardi2@gmail.com>
2019-09-02 12:52:50 +0100

    fix about content in top-content section

--- js/index.js
+++ js/index.js
@@ -58,7 +58,7 @@ document.querySelector("#cta-img").src = siteContent.cta["img-src"];
 document.querySelectorAll(".top-content .text-content h4")[0].textContent = siteContent["main-content"]["features-h4"];
 document.querySelectorAll(".top-content .text-content p")[0].textContent = siteContent["main-content"]["features-content"];
 document.querySelectorAll(".top-content .text-content h4")[1].textContent = siteContent["main-content"]["about-h4"]
-document.querySelectorAll(".top-content .text-content p")[1].textContent = siteContent["main-content"]["about-h4"];
+document.querySelectorAll(".top-content .text-content p")[1].textContent = siteContent["main-content"]["about-content"];
 
 // Update Middle Image
 document.querySelector("#middle-img").src = siteContent["main-content"]["middle-img-src"];
